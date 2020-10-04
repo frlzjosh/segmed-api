42889debd77471285ffa45940290005a3fd9ca79
frlzjosh <joshua.magdaleno.472@my.csun.edu>
2020-10-04 02:10:38 -0700

    Improved the workflow on how photos are created, included cors, added another attribute to make sorting my flagged and non flagged photos in the front-end, and created a new function getFirstUser()

--- main.go
+++ main.go
@@ -4,7 +4,8 @@ import (
 	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/gofiber/fiber"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/jinzhu/gorm"
 	"github.com/segmed-apis/base"
 	"github.com/segmed-apis/models"
@@ -27,10 +28,12 @@ func initRoutes(app *fiber.App) {
 	app.Get("/get/all", models.GetPictures)
 	app.Get("/flag/:id", models.FlagPicture)
 	app.Post("/save/", models.SaveNewPicture)
+	app.Post("/delete", models.DeletePicture)
 }
 
 func main() {
 	app := fiber.New()
+	app.Use(cors.New())
 	initializeDB()
 	defer base.DB.Close()
 	initRoutes(app)
--- models/pictures.go
+++ models/pictures.go
@@ -3,7 +3,7 @@ package models
 import (
 	"log"
 
-	"github.com/gofiber/fiber"
+	"github.com/gofiber/fiber/v2"
 	"github.com/jinzhu/gorm"
 	"github.com/segmed-apis/base"
 )
@@ -12,28 +12,42 @@ type Pictures struct {
 	gorm.Model
 	PictureUrl string `json:"picture_url"`
 	IsFlagged  bool   `json:"is_flagged"`
+	CustomTime string `json:"custom_time"`
 }
 
-func GetPictures(c *fiber.Ctx) {
+func GetPictures(c *fiber.Ctx) error {
 	log.Println("GetPictures()")
 	db := base.DB
 	var pictures []Pictures
 	db.Find(&pictures)
-	c.JSON(pictures)
+	return c.Status(fiber.StatusOK).JSON(pictures)
 }
 
-func FlagPicture(c *fiber.Ctx) {
+func FlagPicture(c *fiber.Ctx) error {
 	log.Println("FlagPicture()")
 	id := c.Params("id")
 	db := base.DB
 	var picture Pictures
 	db.Find(&picture, id)
 	picture.IsFlagged = !picture.IsFlagged
+	firstPicture := getFirstUser()
+	if picture.IsFlagged == false {
+		picture.CustomTime = firstPicture.CreatedAt.String()
+	} else {
+		picture.CustomTime = picture.UpdatedAt.String()
+	}
 	db.Save(&picture)
-	c.JSON(picture)
+	return c.Status(fiber.StatusOK).JSON(picture)
 }
 
-func SaveNewPicture(c *fiber.Ctx) {
+func getFirstUser() Pictures {
+	db := base.DB
+	var firstPicture Pictures
+	db.First(&firstPicture)
+	return firstPicture
+}
+
+func SaveNewPicture(c *fiber.Ctx) error {
 	log.Println("SaveNewPicture()")
 	params := new(struct {
 		PictureUrl string `json:"picture_url"`
@@ -41,8 +55,21 @@ func SaveNewPicture(c *fiber.Ctx) {
 	c.BodyParser(&params)
 	db := base.DB
 	var picture Pictures
+	firstPicture := getFirstUser()
 	picture.PictureUrl = params.PictureUrl
 	picture.IsFlagged = false
+	picture.CustomTime = firstPicture.CreatedAt.String()
 	db.Create(&picture)
-	c.JSON(picture)
+	return c.Status(fiber.StatusOK).JSON(picture)
+}
+
+func DeletePicture(c *fiber.Ctx) error {
+	log.Println("DeletePicture()")
+	params := new(struct {
+		ID int `json:"ID"`
+	})
+	c.BodyParser(&params)
+	db := base.DB
+	pictures := db.Delete(&Pictures{}, params.ID)
+	return c.JSON(pictures)
 }
